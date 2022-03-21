af07d6cd2447924b7456abd71d6b13830648a568
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-21 08:26:33 +0100

    Aspect Learning

---
+++ src/test/java/com/example/springaspectv1/SpringAspectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaspectv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAspectV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
